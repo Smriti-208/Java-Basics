4718287e33b3b397074a5a6e1dc97666e3be4388
Smriti-208 <simmipriya208@Gmail.com>
2026-08-06 19:42:26 +0000

    Added Conditionals and loops

---
+++ Conditionals.java
@@ -0,0 +1,40 @@
+public class Conditionals{
+public static void main(String[] args){
+    /* 
+
+    Syntax of it statements:
+    if(boolean expression T or F){
+          //body
+       } else {
+           //do this
+       }
+
+       */
+
+
+    //   Single If-Else Example:
+
+    //   int salary = 15400;
+    //   if(salary > 10000) {
+    //     salary = salary + 2000;
+    //   } else {
+    //     salary = salary + 1000;
+    //   }
+
+
+
+    //   Multiple If-Else Example:
+
+    //  int salary = 25400;
+    //   if(salary > 10000){
+    //     salary += 2000; //salary = salary + 2000
+    //   } else if(salary > 20000) {
+    //     salary += 3000;
+    //   } else {
+    //     salary += 1000;
+    //   }
+
+    //   System.out.println(salary);
+
+}
+}
---
+++ Loops.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args){
+
+
+        //FOR LOOP 
+
+        // (you need to run
+        //  for loop when you know
+        //  how many times loop is
+        //  going to run)
+        //  Syntax of for loops:
+
+        //  for(initialisation; condiiton; increment/decrement){
+        //     //body
+        //  }
+
+
+        //Q: Print numbers form 1 to 5
+        //    for(int num = 1; num <= 5; num++){
+        //     System.out.println(num);
+        //    }
+
+
+        //Q: Print numbers from 1 to n
+        // Scanner  sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // for (int num = 1; num <= n; num++) {
+        //      System.out.print(num+" ") ;  
+        // }
+
+
+
+        //WHILE LOOP 
+
+        // (you need to run a while 
+        // loop when you don't know the number
+        //  of iterations or how many times the 
+        // loop is going to run)
+
+        // Syntax of while loop:
+
+        // while(condiiton){
+        //     //body
+        // }
+
+        //Q: Print numbers form 1 to n
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int num = 1;
+        // while(num <= 5){
+        //     System.out.println(num);
+        //     num += 1;
+        // }
+
+
+
+        //DO WHILE LOOP
+
+        // (use this when you 
+        // want to print your output atleast
+        //  once without cheking the condition)
+        // do{
+        //     body
+        // } while (condition);
+
+        //EXAMPLE:
+        // int n = 1;
+        // do{
+        //     System.out.println("Hello World");
+        // } while (n != 1);
+    }
+    
+}
